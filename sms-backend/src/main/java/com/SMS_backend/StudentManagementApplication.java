ce9baa50f7dfaa845398973f6a38dbb98c9d9ff7
Varsha <varshazalte1402@gmail.com>
2025-09-08 17:58:55 +0530

    Added Dockerfile and updated application properties

--- sms-backend/src/main/java/com/SMS_backend/StudentManagementApplication.java
+++ sms-backend/src/main/java/com/SMS_backend/StudentManagementApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentManagementApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(StudentManagementApplication.class, args);
+
+        
     }
 }
