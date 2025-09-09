bbf1e6d9074e38c418d55eb11ded5d3e9694efab
varsha-1402 <varshazalte1402@gmail.com>
2025-09-09 19:50:15 +0530

    Update StudentList.js

--- sms-frontend/src/components/StudentList.js
+++ sms-frontend/src/components/StudentList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://studentmanagementsystem-production-6a5a.up.railway.app/api/students";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://studentmanagementsystem-production-6a5a.up.railway.app";
 
 const StudentList = () => {
   const [students, setStudents] = useState([]);
