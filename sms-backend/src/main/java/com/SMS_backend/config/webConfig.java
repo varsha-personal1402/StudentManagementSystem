e77f70d40e7aac93aba2da2de8a3ebf08e8c374e
varsha-1402 <varshazalte1402@gmail.com>
2025-09-09 14:58:28 +0530

    Update webConfig.java

--- sms-backend/src/main/java/com/SMS_backend/config/webConfig.java
+++ sms-backend/src/main/java/com/SMS_backend/config/webConfig.java
@@ -13,7 +13,7 @@ public class webConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
