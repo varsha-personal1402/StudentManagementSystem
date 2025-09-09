4434141972e8d38a812619cdcb603f884c44996c
varsha-1402 <varshazalte1402@gmail.com>
2025-09-10 01:21:41 +0530

    Update SecurityConfig.java

--- sms-backend/src/main/java/com/SMS_backend/config/SecurityConfig.java
+++ sms-backend/src/main/java/com/SMS_backend/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+package com.SMS_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
